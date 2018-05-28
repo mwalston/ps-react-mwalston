24e1433774f6305ebd7118956f06666cf9f12746
Michael Walston <michael.walston@hotmail.comm>
2018-05-28 18:51:24 -0400

    Hello World Component

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// @ts-ignore
+import HelloWorld from 'ps-react/HelloWorld';
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
