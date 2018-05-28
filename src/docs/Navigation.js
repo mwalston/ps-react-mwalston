2bf7ceb4d44411ea4f11c60bf850e86f57153b41
Michael Walston <michael.walston@hotmail.comm>
2018-05-28 18:26:52 -0400

    docs

---
+++ src/docs/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Navigation({components}) {
+  return (
+    <ul className="navigation">
+      {
+        components.map( name => {
+          return (
+            <li key={name}>
+              <a href={`#${name}`}>{name}</a>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
+}
+
+Navigation.propTypes = {
+  components: PropTypes.array.isRequired
+};
+
+export default Navigation;
